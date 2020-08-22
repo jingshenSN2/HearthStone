42cc385ff82565a3ad56da3723f42f9f43e7927b
jingshenSN2 <512320254@qq.com>
2020-08-22 23:00:03 +0800

    item registry update

--- src/main/java/sn2/hearthstone/item/ItemRegistry.java
+++ src/main/java/sn2/hearthstone/item/ItemRegistry.java
@@ -8,13 +8,14 @@ import sn2.hearthstone.HearthStone;
 
 public class ItemRegistry {
 
-	public static final Item SIMPLE_OVERWORLD_HEARTHSTONE = new ItemHearthStoneWorld(20*60*20, 0, World.OVERWORLD, 500);
-	public static final Item SIMPLE_NETHER_HEARTHSTONE = new ItemHearthStoneWorld(20*60*20, 1, World.NETHER, 500);
-	public static final Item SIMPLE_END_HEARTHSTONE = new ItemHearthStoneWorld(20*60*20, 2, World.END, 500);
-	public static final Item OVERWORLD_HEARTHSTONE = new ItemHearthStoneWorld(20*60*20, 3, World.OVERWORLD, -1);
-	public static final Item NETHER_HEARTHSTONE = new ItemHearthStoneWorld(20*60*20, 4, World.NETHER, -1);
-	public static final Item END_HEARTHSTONE = new ItemHearthStoneWorld(20*60*20, 5, World.END, -1);
-	public static final Item MINER_HEARTHSTONE = new ItemHearthStoneMiner(20*60*20, 6);
+	private static int minTick = 20*60;
+	public static final Item SIMPLE_OVERWORLD_HEARTHSTONE = new ItemHearthStoneWorld(30 * minTick, 0, World.OVERWORLD, 500);
+	public static final Item SIMPLE_NETHER_HEARTHSTONE = new ItemHearthStoneWorld(30 * minTick, 1, World.NETHER, 500);
+	public static final Item SIMPLE_END_HEARTHSTONE = new ItemHearthStoneWorld(30 * minTick, 2, World.END, 500);
+	public static final Item OVERWORLD_HEARTHSTONE = new ItemHearthStoneWorld(20 * minTick, 3, World.OVERWORLD, -1);
+	public static final Item NETHER_HEARTHSTONE = new ItemHearthStoneWorld(20 * minTick, 4, World.NETHER, -1);
+	public static final Item END_HEARTHSTONE = new ItemHearthStoneWorld(20 * minTick, 5, World.END, -1);
+	public static final Item MINER_HEARTHSTONE = new ItemHearthStoneMiner(20 * minTick, 6);
 
 	public static void init() {
 		Registry.register(Registry.ITEM, new Identifier(HearthStone.MODID, "simple_overworld_hearthstone"), SIMPLE_OVERWORLD_HEARTHSTONE);
@@ -23,5 +24,6 @@ public class ItemRegistry {
 		Registry.register(Registry.ITEM, new Identifier(HearthStone.MODID, "overworld_hearthstone"), OVERWORLD_HEARTHSTONE);
 		Registry.register(Registry.ITEM, new Identifier(HearthStone.MODID, "nether_hearthstone"), NETHER_HEARTHSTONE);
 		Registry.register(Registry.ITEM, new Identifier(HearthStone.MODID, "end_hearthstone"), END_HEARTHSTONE);
+		Registry.register(Registry.ITEM, new Identifier(HearthStone.MODID, "miner_hearthstone"), MINER_HEARTHSTONE);
 	}
 }
